51486cb1c08817446f73add734a8b971098d6d8b
Ayden939 <212651389+Ayden939@users.noreply.github.com>
2025-12-10 21:11:36 -0600

    Make game playable

--- main.py
+++ main.py
@@ -7,14 +7,28 @@ hero = Character("Lady Samantha Rostnovak", 1)
 enemy = Skeleton()
 
 while hero.health > 0 and enemy.health > 0:
-    hero.attack(enemy)
-    print(f"Enemy hp: {enemy.health}")
-    if(enemy.health <= 0):
-        print("Enemy defeated!")
+
+    choice =  input("Please choose attack, retreat, or heal:    ")
+    if(choice == "attack"):
+        hero.attack(enemy)
+        print(f"Enemy hp: {enemy.health}")
+        if(enemy.health <= 0):
+            print("Enemy defeated!")
+            break
+        enemy.attack(hero)
+        print(f"Hero hp: {hero.health}")
+        if(hero.health <= 0):
+            print("Defeated")
+            break
+        
+    elif(choice == "retreat"):
+        print(f"{hero.name} has left the dungeon")
         break
-    enemy.attack(hero)
-    print(f"Hero hp: {hero.health}")
-    if(hero.health <= 0):
-        print("Defeated")
-        break
+
+    elif(choice == "heal"):
+        hero.heal(5)
+        print(f"Hero hp: {hero.health}")
     
+    else:
+        print("You must attack, retreat, or heal:   ")
+        
